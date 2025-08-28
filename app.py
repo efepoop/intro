000922290f53ef682e3a22083245be5192993d29
EL FELI <142848273+efepoop@users.noreply.github.com>
2025-08-28 09:59:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from PIL import Image
 
+st.title("Los tralas")
 
-st.title ("Los tralas")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Valerina.png')
+
+st.image(image, caption='interfaces multimodales')
